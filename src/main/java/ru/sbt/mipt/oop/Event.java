1796d4668dce7fff363e3104099a0072cc8f521c
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-25 00:22:52 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/Event.java
+++ src/main/java/ru/sbt/mipt/oop/Event.java
@@ -1,23 +1,28 @@
 package ru.sbt.mipt.oop;
 
-import static ru.sbt.mipt.oop.SensorEventTypeLight.*;
-import static ru.sbt.mipt.oop.SensorEventTypeDoor.*;
+import static ru.sbt.mipt.oop.SensorEventType.*;
+import static ru.sbt.mipt.oop.SensorEventType.*;
 
 public class Event {
     private static SensorEvent event;
 
     public Event (){
         RandomEvent tempEvent = new RandomEvent();
-        this.event = tempEvent.randomData();
+        this.event = tempEvent.randomizeData();
     }
     public Event(SensorEvent _event){
         this.event = _event;
     }
 
-    public SensorEvent getEvent(){
+    public SensorEvent getNextSensorEvent(){
+        RandomEvent tempEvent = new RandomEvent();
+        event = tempEvent.randomizeData();
+        return event;
+    }
+    public SensorEvent getSensorEvent(){
         return this.event;
     }
-    public void setEvent(SensorEvent newEvent){
+    public void setSensorEvent(SensorEvent newEvent){
         this.event = newEvent;
     }
 }
