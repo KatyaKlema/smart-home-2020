718a12196191eed4f37d8ad55899b19d212edc68
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-25 00:26:55 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/RandomEvent.java
+++ src/main/java/ru/sbt/mipt/oop/RandomEvent.java
@@ -1,19 +1,18 @@
 package ru.sbt.mipt.oop;
 
 public class RandomEvent implements RandomData<SensorEvent> {
-    public SensorEvent randomData(){
+    public SensorEvent randomizeData(){
         Double randomNumber = Math.random();
         if (randomNumber < 0.05)
             return null; // null means end of event stream
-        SensorEventTypeLight  sensorEventTypeLight = null;
-        SensorEventTypeDoor sensorEventTypeDoor = null;
+        SensorEventType  sensorEventType;
 
         if(4 * randomNumber % 2 == 0)
-            sensorEventTypeLight =  SensorEventTypeLight.values()[(int) (4 * randomNumber) % 2];
+            sensorEventType =  SensorEventType.values()[(int) (4 * randomNumber) % 2];
         else
-            sensorEventTypeDoor = SensorEventTypeDoor.values()[(int) (4 * randomNumber) % 2];
+            sensorEventType = SensorEventType.values()[(int) (4 * randomNumber) % 2];
 
         String objectId = "" + ((int) (10 * Math.random()));
-        return new SensorEvent(sensorEventTypeDoor, sensorEventTypeLight, objectId);
+        return new SensorEvent(sensorEventType, objectId);
     }
 }
