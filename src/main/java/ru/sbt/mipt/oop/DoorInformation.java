29fcdcde25e0d901267a1f9710899015b06aafd9
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-25 00:22:40 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/DoorInformation.java
+++ src/main/java/ru/sbt/mipt/oop/DoorInformation.java
@@ -1,14 +1,10 @@
 package ru.sbt.mipt.oop;
 
-import static ru.sbt.mipt.oop.SensorEventTypeDoor.DOOR_OPEN;
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_OPEN;
 
 public class DoorInformation {
-    private void sendCommand(SensorCommand command) {
-        System.out.println("Pretent we're sending command " + command);
-    }
-
     public void toggleDoorLight(Event event, Door door, SmartHome smartHome, Room room){
-        if (event.getEvent().getTypeDoor() == DOOR_OPEN) {
+        if (event.getSensorEvent().getType() == DOOR_OPEN) {
             door.setOpen(true);
             System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
         } else {
@@ -16,15 +12,9 @@ public class DoorInformation {
             System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
             // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
             // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
-            if (room.getName().equals("hall")) {
-                for (Room homeRoom : smartHome.getRooms()) {
-                    for (Light light : homeRoom.getLights()) {
-                        light.setOn(false);
-                        SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
-                        sendCommand(command);
-                    }
-                }
-            }
+            HallDoorIterator hallDoorIterator = new HallDoorIterator(smartHome);
+            HallDoorEventProcessor hallDoorEventProcessor = new HallDoorEventProcessor(smartHome);
+            hallDoorEventProcessor.processing(room);
         }
     }
 }
