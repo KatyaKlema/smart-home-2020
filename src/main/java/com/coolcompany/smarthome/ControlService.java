351cfda5c7fdb793ac9dd15836028c5f50fd0533
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-26 03:50:44 +0800

    Add files via upload

---
+++ src/main/java/com/coolcompany/smarthome/ControlService.java
@@ -0,0 +1,13 @@
+package com.coolcompany.smarthome;
+import ru.sbt.mipt.oop.AdaptController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControlService {
+    List<AdaptController> adaptControllerList = new ArrayList<>();
+    public void registerAdaptController(AdaptController adaptController, String id) {
+    };
+}
+
+
