be92637d540925658c9582b8bbaa1b1a22b12f8c
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-01 21:38:05 +0300

    Delete SendCommand.java

--- src/main/java/ru/sbt/mipt/oop/send_command/SendCommand.java
+++
@@ -1,9 +0,0 @@
-package ru.sbt.mipt.oop.send_command;
-
-import ru.sbt.mipt.oop.sensor_command.SensorCommand;
-
-public class SendCommand {
-    public SendCommand(SensorCommand command){
-        System.out.println("Pretent we're sending command " + command);
-    }
-}
