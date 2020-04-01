7743d49c6bbd5d4496d8fcc862133c33a811cc90
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-02 03:56:14 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/remote/RemoteController.java
+++ src/main/java/ru/sbt/mipt/oop/remote/RemoteController.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 import rc.RemoteControl;
 import ru.sbt.mipt.oop.button.Button;
+import ru.sbt.mipt.oop.home_components.SmartHome;
+import ru.sbt.mipt.oop.remote_instructions.*;
+
 public class RemoteController implements RemoteControl {
     private final String[] buttons = { "1", "2", "3", "4","A", "B", "C", "D"};
     private String rcId;
@@ -17,7 +20,33 @@ public class RemoteController implements RemoteControl {
             buttonMap.put(buttonId, new Button(buttonId));
         }
     }
+    public void smartHomeRemote(SmartHome smartHome){
+        Instruction hallDoorCloseInstruction = new HallDoorCloseInstruction(smartHome);
+        Instruction allLightOnInstruction = new AllLightOnInstruction(smartHome);
+        Instruction hallLightOnInstruction = new HallLightOnInstruction(smartHome);
+        Instruction allLightOffInstruction = new AllLightOffInstruction(smartHome);
+        Instruction alarmActivateInstruction = new AlarmActivateInstruction(smartHome, 1);
+        Instruction triggerAlarmInstruction = new TriggerAlarmInstruction(smartHome);
 
+        Instruction nothing = new Nothing();
+
+        Instruction[] commands = new Instruction[]{hallDoorCloseInstruction, allLightOnInstruction, hallLightOnInstruction,
+                allLightOffInstruction, alarmActivateInstruction, triggerAlarmInstruction};
+        //по идее лишняя проверка, но на будущее
+        if (commands.length > buttons.length) {
+            System.out.println(String.format("your remote can bind only %s commands", buttons.length ));
+            return;
+        }
+
+        for (int i = 0; i < commands.length; i++) { // остальные копки остануться пустыми
+            //по идее лишняя проверка, но на будущее
+            if (buttonMap.get(buttons[i]) == null) {
+                System.out.println("there is not such button");
+                continue;
+            }
+            buttonMap.get(buttons[i]).setInstruction(commands[i]);
+        }
+    }
     public String getId(){
         return this.rcId;
     }
