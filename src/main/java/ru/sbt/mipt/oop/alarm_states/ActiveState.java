31c8b131f13dd19e7213f0eb2d3c18c3dfacd698
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-09 05:39:21 +0800

    Add files via upload

---
+++ src/main/java/ru/sbt/mipt/oop/alarm_states/ActiveState.java
@@ -0,0 +1,47 @@
+package ru.sbt.mipt.oop.alarm_states;
+
+import ru.sbt.mipt.oop.alarm.Alarm;
+import ru.sbt.mipt.oop.alarm.AlarmState;
+import ru.sbt.mipt.oop.alarm.AlarmStateType;
+
+public class ActiveState extends AlarmState {
+    public ActiveState(Alarm alarm){
+        this.alarmStateType = AlarmStateType.ACTIVE;
+        this.alarm = alarm;
+    }
+
+    public ActiveState(){
+        this.alarmStateType = AlarmStateType.ACTIVE;
+    }
+
+    @Override
+    public void alarmActivate(Integer code) {
+        if(!isIgnore) {
+            if (isAlarm()) {
+                System.out.println("Already the Alarm has been activated");
+            }
+        }
+    }
+
+    @Override
+    public void alarmDeactivate(Integer code) {
+        if(!isIgnore) {
+            if (alarm.isEqual(code)) {
+                alarm.setAlarmState(alarm, new DeactiveState(alarm));
+            } else {
+                alarm.setAlarmState(alarm, new TriggerState(alarm));
+                System.out.print("TRIGGER !");
+            }
+        }
+    }
+
+    @Override
+    public void alarmTrigger() {
+        if(!isIgnore) {
+            if (isAlarm()) {
+                alarm.setAlarmState(alarm, new TriggerState(alarm));
+                System.out.println("TRIGGER !");
+            }
+        }
+    }
+}
