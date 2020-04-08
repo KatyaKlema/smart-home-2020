25814e43cf2872701e25c4bbbba8e9ea868ca8a3
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-09 05:40:17 +0800

    Add files via upload

---
+++ src/main/java/ru/sbt/mipt/oop/processors/TriggerAlarmEventProcessor.java
@@ -0,0 +1,42 @@
+package ru.sbt.mipt.oop.processors;
+
+import ru.sbt.mipt.oop.events.SensorEvent;
+import ru.sbt.mipt.oop.events.SensorEventType;
+import ru.sbt.mipt.oop.home_components.SmartHome;
+
+public class TriggerAlarmEventProcessor implements Processor{
+    private SmartHome smartHome;
+    private boolean isSended;
+    private Processor wrapperProcessor;
+    public TriggerAlarmEventProcessor(SmartHome smartHome, Processor wrapperProcessor){
+        this.smartHome = smartHome;
+        this.wrapperProcessor = wrapperProcessor;
+        isSended = false;
+    }
+    private boolean isCorrectType(SensorEvent event){
+        boolean ret = false;
+        SensorEventType fixedType = event.getType();
+        for(SensorEventType sensorEventType : SensorEventType.values()){
+            ret = ret || (fixedType == sensorEventType);
+        }
+        return ret;
+    }
+
+    public void processing(SensorEvent event){
+        if(isCorrectType(event)){
+            if(smartHome.getAlarm().isActivatedAlarm()){
+                smartHome.getAlarm().getAlarmState().alarmTrigger();
+                System.out.println("Sending sms");
+            }
+            else if(smartHome.getAlarm().isTriggered()){
+                smartHome.getAlarm().getAlarmState().ignore();
+                if(!isSended)
+                    System.out.println("Sending sms");
+                isSended = true;
+            }
+            else{
+                wrapperProcessor.processing(event);
+            }
+        }
+    }
+}
