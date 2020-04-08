02084f9df2cdd4aa3d6c14c9c830d8243a4f6718
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-09 05:38:23 +0800

    Add files via upload

---
+++ src/main/java/ru/sbt/mipt/oop/alarm/AlarmState.java
@@ -0,0 +1,49 @@
+package ru.sbt.mipt.oop.alarm;
+
+public abstract class AlarmState {
+    protected Alarm alarm;
+    protected AlarmStateType alarmStateType;
+    public boolean isIgnore = false;
+
+    public abstract void alarmActivate(Integer instruction);
+    public abstract void alarmDeactivate(Integer instruction);
+    public abstract void alarmTrigger();
+    public void ignore(){
+        this.isIgnore = true;
+    }
+
+    public AlarmStateType getAlarmStateType(){
+        return this.alarmStateType;
+    }
+
+    public Alarm getAlarm(){
+        return this.alarm;
+    }
+
+    public boolean isSame(AlarmState otherAlarmState){
+        return this.alarm == otherAlarmState.getAlarm() && this.alarmStateType == otherAlarmState.getAlarmStateType();
+    }
+
+    public boolean equals(Object object){
+        if(this == object){
+            return true;
+        }
+        else if(object instanceof AlarmState){
+            AlarmState otherAlarmState = (AlarmState) object;
+            return this.isSame(otherAlarmState);
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isAlarm(){
+        if (this.alarm == null) {
+            System.out.println("Error: Alarm is empty");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
