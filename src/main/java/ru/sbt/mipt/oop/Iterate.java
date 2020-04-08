cb968dff6d88da56334186d87c772bbe4f1c4922
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-08 23:11:34 +0300

    Delete Iterate.java

--- src/main/java/ru/sbt/mipt/oop/Iterate.java
+++
@@ -1,9 +0,0 @@
-package ru.sbt.mipt.oop;
-
-public class Iterate {
-    public SensorEvent getNextSensorEvent(SensorEvent event){
-        RandomEventGenerate tempEvent = new RandomEventGenerate();
-        event = tempEvent.randomizeData();
-        return event;
-    }
-}
