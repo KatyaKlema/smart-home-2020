b930fe09217b54318e4e7c0bdaa3007fd73541b4
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-25 17:23:35 +0300

    Update Application.java

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -2,11 +2,6 @@ package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.ControlService;
 
-import com.coolcompany.smarthome.events.SensorEventsManager;
-import com.google.gson.Gson;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -14,30 +9,29 @@ import java.util.List;
 import ru.sbt.mipt.oop.alarm.Alarm;
 import ru.sbt.mipt.oop.data_reader.JSONData;
 import ru.sbt.mipt.oop.home_components.SmartHome;
+import ru.sbt.mipt.oop.home_readers.HomeReader;
+import ru.sbt.mipt.oop.home_readers.JSONHomeReader;
 import ru.sbt.mipt.oop.processors.*;
 
 import ru.sbt.mipt.oop.events.SensorEvent;
 import ru.sbt.mipt.oop.remote.RemoteController;
-import ru.sbt.mipt.oop.smart_home.SmartHomeHandler;
-
 public class Application {
+    public static HomeReader homeReader = new JSONHomeReader();
     private static ControlService controlService;
     public Application(ControlService controlService){
         this.controlService = controlService;
     }
     public static void main(String... args) throws IOException {
         // считываем состояние дома из файла
-        JSONData tempJSON = new JSONData("smart-home-1.js");
-        Gson gson = new Gson();
-        SmartHome smartHome = gson.fromJson(tempJSON.getData(), SmartHome.class);
+        SmartHome smartHome = homeReader.read("smart-home-1.js");
         smartHome.setAlarm(new Alarm(2));
-
-        TriggerAlarmEventProcessor triggerAlarmEventProcessorLight = new TriggerAlarmEventProcessor(smartHome, new LightEventProcessor(smartHome));
-        TriggerAlarmEventProcessor triggerAlarmEventProcessorDoor = new TriggerAlarmEventProcessor(smartHome, new DoorEventProcessor(smartHome));
 
         // начинаем цикл обработки событий
         SensorEvent event = new SensorEvent();
-        List<Processor> processors = Arrays.asList(new LightEventProcessor(smartHome), new DoorEventProcessor(smartHome), new HallDoorEventProcessor(smartHome));
+        List<Processor> processors = Arrays.asList(new LightEventProcessor(smartHome), new DoorEventProcessor(smartHome),
+                                    new HallDoorEventProcessor(smartHome), new TriggerAlarmEventProcessor(smartHome,
+                                    Arrays.asList(new LightEventProcessor(smartHome), new DoorEventProcessor(smartHome))));
+
         SmartHomeHandler smartHomeHandler = new SmartHomeHandler(smartHome, event, processors);
         smartHomeHandler.runCycleForEvent();
 
