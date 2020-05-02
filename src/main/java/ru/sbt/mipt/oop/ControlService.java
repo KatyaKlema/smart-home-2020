68f17ed3a70660426b6a310c475550260257d15a
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-05-02 21:35:00 +0800

    Add files via upload

---
+++ src/main/java/ru/sbt/mipt/oop/ControlService.java
@@ -0,0 +1,15 @@
+package ru.sbt.mipt.oop;
+
+
+import ru.sbt.mipt.oop.remote.RemoteController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControlService {
+    List<RemoteController> adaptControllerList = new ArrayList<>();
+    public void registerAdaptController(RemoteController adaptController, String id) {
+    };
+}
+
+
