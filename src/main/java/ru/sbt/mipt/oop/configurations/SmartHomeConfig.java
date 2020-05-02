e6280554f49858ed88dbe3684d80e0a9ba5ef5ff
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-05-02 17:25:30 +0300

    Update SmartHomeConfig.java

--- src/main/java/ru/sbt/mipt/oop/configurations/SmartHomeConfig.java
+++ src/main/java/ru/sbt/mipt/oop/configurations/SmartHomeConfig.java
@@ -1,70 +1,36 @@
 package ru.sbt.mipt.oop.configurations;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
-import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import ru.sbt.mipt.oop.adapter.SmartHomeAdapter;
-import ru.sbt.mipt.oop.data_reader.JSONData;
-import ru.sbt.mipt.oop.events.SensorEvent;
 import ru.sbt.mipt.oop.home_components.SmartHome;
+import ru.sbt.mipt.oop.home_readers.HomeReader;
 import ru.sbt.mipt.oop.processors.*;
-import ru.sbt.mipt.oop.smart_home.SmartHomeHandler;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 
 public class SmartHomeConfig {
     @Bean
-    public SmartHome smartHome() throws IOException{
-        JSONData tempJSON = new JSONData("smart-home-1.js");
-        Gson gson = new Gson();
-        SmartHome smartHome = gson.fromJson(tempJSON.getData(), SmartHome.class);
-        return smartHome;
+    public SmartHome smartHome(HomeReader homeReader) throws IOException{
+        return homeReader.read("smart-home-1.js");
     }
-
-    @Bean
-    TriggerAlarmEventProcessor triggerAlarmEventProcessorLight(SmartHome smartHome){
-        return new TriggerAlarmEventProcessor(smartHome, new LightEventProcessor(smartHome));
+    
+    @Bean 
+    public List<Processor> getProcessers(SmartHome smartHome){
+        return Arrays.asList(new LightEventProcessor(smartHome), new DoorEventProcessor(smartHome),
+                new HallDoorEventProcessor(smartHome));
     }
-
     @Bean
-    TriggerAlarmEventProcessor triggerAlarmEventProcessorDoor(SmartHome smartHome){
-        return new TriggerAlarmEventProcessor(smartHome, new DoorEventProcessor(smartHome));
+    public SmartHomeAdapter smartHomeAdapter(List<Processor> processors){
+        return new SmartHomeAdapter(processors);
     }
-
     @Bean
-    public SensorEvent sensorEvent(){
-        return new SensorEvent();
-    }
-
-    @Bean
-    public Processor doorEventProcessor(){
-        return new DoorEventProcessor();
-    }
-
-    @Bean
-    public Processor lightEventProcessor(){
-        return new LightEventProcessor();
-    }
-
-    @Bean
-    public Processor hallDoorEventProcessor(){
-        return new HallDoorEventProcessor();
-    }
-
-    @Bean
-    public SmartHomeHandler smartHomeHandler(SmartHome smartHome, SensorEvent event, List<Processor> processors){
-        return new SmartHomeHandler(smartHome, event, processors);
-    }
-
-    @Bean
-    public SensorEventsManager smartHomeAdapter(SmartHomeAdapter smartHomeAdapter){
+    public SensorEventsManager sensorEventManager(SmartHomeAdapter smartHomeAdapter){
         SensorEventsManager sensorEventsManager = new SensorEventsManager();
         sensorEventsManager.registerEventHandler(smartHomeAdapter);
         return sensorEventsManager;
     }
-
-
 }
