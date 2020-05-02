8f86ad00bd0094b73b5dca461e4cb597bf5a043a
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-05-02 16:48:24 +0300

    Update SmartHomeConfig.java

--- src/main/java/ru/sbt/mipt/oop/configurations/SmartHomeConfig.java
+++ src/main/java/ru/sbt/mipt/oop/configurations/SmartHomeConfig.java
@@ -1,53 +1,36 @@
 package ru.sbt.mipt.oop.configurations;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
-import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import ru.sbt.mipt.oop.adapter.SmartHomeAdapter;
-import ru.sbt.mipt.oop.data_reader.JSONData;
-import ru.sbt.mipt.oop.events.SensorEvent;
 import ru.sbt.mipt.oop.home_components.SmartHome;
 import ru.sbt.mipt.oop.home_readers.HomeReader;
 import ru.sbt.mipt.oop.processors.*;
-import ru.sbt.mipt.oop.smart_home.SmartHomeHandler;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
-@Configuration
+
 public class SmartHomeConfig {
     @Bean
-    public SmartHome smartHome(HomeReader homeReader){
+    public SmartHome smartHome(HomeReader homeReader) throws IOException{
         return homeReader.read("smart-home-1.js");
     }
-
-    @Bean
-    public SensorEvent sensorEvent(){
-        return new SensorEvent();
+    
+    @Bean 
+    public List<Processor> getProcessers(SmartHome smartHome){
+        return Arrays.asList(new LightEventProcessor(smartHome), new DoorEventProcessor(smartHome),
+                new HallDoorEventProcessor(smartHome));
     }
-
     @Bean
-    public Processor doorEventProcessor(){
-        return new DoorEventProcessor();
+    public SmartHomeAdapter smartHomeAdapter(List<Processor> processors){
+        return new SmartHomeAdapter(processors);
     }
-
-    @Bean
-    public Processor lightEventProcessor(){
-        return new LightEventProcessor();
-    }
-
-    @Bean
-    public Processor hallDoorEventProcessor(){
-        return new HallDoorEventProcessor();
-    }
-
     @Bean
     public SensorEventsManager sensorEventManager(SmartHomeAdapter smartHomeAdapter){
         SensorEventsManager sensorEventsManager = new SensorEventsManager();
         sensorEventsManager.registerEventHandler(smartHomeAdapter);
         return sensorEventsManager;
     }
-
-
 }
